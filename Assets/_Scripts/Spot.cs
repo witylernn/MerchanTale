408da4bf9399e73485f12f476840ffe4311da5c1
witylernn <witylernn@gmail.com>
2017-09-28 15:24:01 -0500

    Test1

---
+++ Assets/_Scripts/Spot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DoozyUI;
+
+public class Spot : MonoBehaviour, IPointerClickHandler
+{
+    public static GameObject parent;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (PlantCheck())
+        {
+            PlantingManager.PickPlant(this.gameObject);
+        }
+        else
+        {
+            parent = this.gameObject;
+            print("Spot empty");
+            UIManager.ShowUiElement("PlantPicker", "UI");
+        }
+    }
+
+    bool PlantCheck()
+    {
+        //if the spot has a child then there is already a plant present.
+        bool planted;
+        if (this.gameObject.transform.childCount > 0)
+        {
+            planted = true;
+        }
+        else
+        {
+            planted = false;
+        }
+        return planted;
+    }
+}
